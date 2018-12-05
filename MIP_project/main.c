4fa86fe25e0ddb461d0e5e9fb83567a69be610e2
Natália Kňažeková <natalia@MacBook-Air-Natalia.local>
2018-12-05 22:05:25 +0100

    Initial Commit

---
+++ MIP_project/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  MIP_project
+//
+//  Created by Natália Kňažeková on 05/12/2018.
+//  Copyright © 2018 Natália Kňažeková. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
