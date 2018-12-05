353f0a68c67b8ac9cf06e47667c289e262d220fe
Natália Kňažeková <natalia@MacBook-Air-Natalia.local>
2018-12-05 22:07:17 +0100

    Základná štruktúra

--- MIP_project/main.c
+++ MIP_project/main.c
@@ -1,15 +1,48 @@
-//
-//  main.c
-//  MIP_project
-//
-//  Created by Natália Kňažeková on 05/12/2018.
-//  Copyright © 2018 Natália Kňažeková. All rights reserved.
-//
-
 #include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+
+int main()                                   //HLAVNY PROGRAM MAIN
+{
+    char c;
+    
+    scanf("%c",&c);                         // cyklus pokiaľ nestlačíme 'k'
+    if (c!='k')
+    {
+        while(c!='k')
+        {
+            switch (c)
+            {
+                case 'm':    //mocnina
+                    break;
+                    
+                case 'n':     //nasobok
+                   
+                    break;
+                    
+                case 't':     //tyzdenna mzda
+               
+                    break;
+                    
+                case 'h':   //histogram
+                   
+                    break;
+                    
+                    
+                    
+            }
+            scanf("%c",&c);                   // vykoná funkciu a znova načíta ďalšie písmeno
+        }
+        
+    }
+    
+    if ((c='k'))                  // ak písmeno 'k' ukončíme program vo funkcii
+    {
+       
+    }
+    
+    
     return 0;
 }
+
